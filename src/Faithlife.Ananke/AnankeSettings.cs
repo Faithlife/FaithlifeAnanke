6159c008226b0cc0f302307ee7f5561238f67e0a
Stephen Cleary <stephen.cleary@faithlife.com>
2018-05-25 14:16:13 -0400

    Remove AnankeSettings.ConsoleLog.

--- src/Faithlife.Ananke/AnankeSettings.cs
+++ src/Faithlife.Ananke/AnankeSettings.cs
@@ -42,11 +42,6 @@ namespace Faithlife.Ananke
 		public StdoutParser StdoutParser { get; }
 
 		/// <summary>
-		/// Service that writes strings to the console. The strings passed to this log will not contain EOL characters.
-		/// </summary>
-		internal IStringLog ConsoleLog { get; }
-
-		/// <summary>
 		/// Service that exits the entire process.
 		/// </summary>
 		internal IExitProcessService ExitProcessService { get; }
@@ -60,7 +55,7 @@ namespace Faithlife.Ananke
 		/// Creates an instance of <see cref="AnankeSettings"/>, with default settings for any setting not specified.
 		/// </summary>
 		/// <param name="maximumRuntime">The amount of time application code should run until it is requested to exit. Defaults to infinite, but most apps should use a non-infinite time.</param>
-		/// <param name="loggerFactory">The core logging factory used by all structured logging. Defaults to a logging factory with a single provider that writes formatted text to <see cref="ConsoleLog"/>.</param>
+		/// <param name="loggerFactory">The core logging factory used by all structured logging. Defaults to a logging factory with a single provider that writes formatted text to the console.</param>
 		/// <param name="loggerIsEnabledFilter">The filter used by the <see cref="AnankeLoggerProvider"/> if <paramref name="loggerFactory"/> is <c>null</c>.</param>
 		/// <param name="loggerFormatter">The formatter used by the <see cref="AnankeLoggerProvider"/> if <paramref name="loggerFactory"/> is <c>null</c>.</param>
 		/// <param name="exitTimeout">The amount of time application code has after it is requested to exit, before the process forcibly exits. Defaults to 10 seconds.</param>
@@ -103,14 +98,13 @@ namespace Faithlife.Ananke
 				exitTimeout ?? TimeSpan.FromSeconds(10),
 				randomMaximumRuntimeRelativeDelta ?? 0.10,
 				stdoutParser ?? ((message, provider) => provider.CreateLogger("App").LogInformation(message)),
-				consoleLog,
 				exitProcessService ?? new ExitProcessService(),
 				signalService);
 		}
 
 		private AnankeSettings(TimeSpan maximumRuntime, ILoggerFactory loggerFactory, TimeSpan exitTimeout,
 			double randomMaximumRuntimeRelativeDelta,
-			StdoutParser stdoutParser, IStringLog consoleLog, IExitProcessService exitProcessService,
+			StdoutParser stdoutParser, IExitProcessService exitProcessService,
 			ISignalService signalService)
 		{
 			MaximumRuntime = maximumRuntime;
@@ -118,7 +112,6 @@ namespace Faithlife.Ananke
 			ExitTimeout = exitTimeout;
 			RandomMaximumRuntimeRelativeDelta = randomMaximumRuntimeRelativeDelta;
 			StdoutParser = stdoutParser;
-			ConsoleLog = consoleLog;
 			ExitProcessService = exitProcessService;
 			SignalService = signalService;
 		}
